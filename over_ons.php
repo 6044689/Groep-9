774cd9d75a14bb2ab1119d41856bcdcaa2cc83e2
piet <6044689@mborijnland.nl>
2025-05-21 10:26:41 +0200

    over ons toegevoeg

---
+++ over_ons.php
@@ -0,0 +1,14 @@
+<?php require_once 'header.php'; ?>
+<body>
+    <main>
+        <section>
+            <video class="skycolor" autoplay loop muted src = "video/Skycolor.mp4"></video>
+            <article>
+                <h1 class="welkom">Menu</h1>
+            </article>
+
+            <article class="Block">
+                <p id="Head">Welcome bij PixelGame</p>
+                <p id="info">
+                    Welkom bij PixelGame, waar we de wereld van gaming en creativiteit samenbrengen! Bij ons vind je een breed scala aan games die je uitdagen, vermaken en inspireren. Of je nu een doorgewinterde gamer bent of net begint, er is voor elk wat wils.
+                    <?php
